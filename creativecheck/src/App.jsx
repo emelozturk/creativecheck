e2f78a244aba1d8421027e2005c17f6d4efe7cdb
emelozturk <oztremel@gmail.com>
2026-05-21 19:54:26 +0100

    Update App.jsx

--- creativecheck/src/App.jsx
+++ creativecheck/src/App.jsx
@@ -5,19 +5,29 @@ import CategoryChips from './components/CategoryChips'
 import ExploreSection from './components/ExploreSection'
 import FeatureStrip from './components/FeatureStrip'
 import Footer from './components/Footer'
+import AddProfilePage from './components/AddProfilePage'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#fdf8f8]" style={{ fontFamily:"'Plus Jakarta Sans', sans-serif" }}>
+    <div
+      className="min-h-screen bg-[#fdf8f8]"
+      style={{ fontFamily:"'Plus Jakarta Sans', sans-serif" }}
+    >
+
       <Navbar />
+
       <main>
         <Hero />
         <SearchBar />
         <CategoryChips />
         <ExploreSection />
         <FeatureStrip />
+
+        <AddProfilePage />
       </main>
+
       <Footer />
+
     </div>
   )
 }
