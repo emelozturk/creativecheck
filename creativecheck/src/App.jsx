25345a4612969f693996d0056cb2cbf055bc46af
emelozturk <oztremel@gmail.com>
2026-05-21 19:46:44 +0100

    Update App.jsx

--- creativecheck/src/App.jsx
+++ creativecheck/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import SearchBar from './components/SearchBar'
@@ -7,18 +5,11 @@ import CategoryChips from './components/CategoryChips'
 import ExploreSection from './components/ExploreSection'
 import FeatureStrip from './components/FeatureStrip'
 import Footer from './components/Footer'
-import AddProfileModal from './components/AddProfileModal'
 
 export default function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
   return (
-    <div
-      className="min-h-screen bg-[#fdf8f8]"
-      style={{ fontFamily: "'Plus Jakarta Sans', sans-serif" }}
-    >
-      <Navbar onAddProfile={() => setIsModalOpen(true)} />
-
+    <div className="min-h-screen bg-[#fdf8f8]" style={{ fontFamily:"'Plus Jakarta Sans', sans-serif" }}>
+      <Navbar />
       <main>
         <Hero />
         <SearchBar />
@@ -26,13 +17,7 @@ export default function App() {
         <ExploreSection />
         <FeatureStrip />
       </main>
-
       <Footer />
-
-      <AddProfileModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-      />
     </div>
   )
 }
