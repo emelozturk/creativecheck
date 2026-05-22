e6f40ea2eb663bc9d0bb23e4b728caecb31417d3
emelozturk <oztremel@gmail.com>
2026-05-22 18:15:16 +0100

    Update main.jsx

--- creativecheck/src/main.jsx
+++ creativecheck/src/main.jsx
@@ -1,38 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom'
-
 import App from './App.jsx'
 import './index.css'
-
-function Root() {
-  return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route
-          path="/"
-          element={<App />}
-        />
-
-        <Route
-          path="*"
-          element={<App />}
-        />
-
-      </Routes>
-    </BrowserRouter>
-  )
-}
 
 ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>
 )
