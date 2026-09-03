dec37dbc1768a046358c5516ee656930e826be3d
emelozturk <oztremel@gmail.com>
2026-09-03 11:16:23 +0100

    Add SEO route layer for profiles categories and locations

--- creativecheck/src/main.jsx
+++ creativecheck/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppClean.jsx'
+import { SeoRouter } from './components/SeoRouter.jsx'
 import './index.css'
 import './roadmap-fix.css'
 import './footer-fix.css'
@@ -10,4 +11,10 @@ import './pastel-palette.css'
 import './pastel-navy-final.css'
 import './footer-social-fix.css'
 import './hero-final-fix.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+
+function Root(){
+  const seoPage=SeoRouter()
+  return seoPage || <App />
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><Root /></React.StrictMode>)
