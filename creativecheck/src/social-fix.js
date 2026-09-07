8ed52b0fab540f0ff83b566479be557dc61deff8
emelozturk <oztremel@gmail.com>
2026-09-07 17:46:34 +0100

    Restore CreativeCheck social media links

--- creativecheck/src/social-fix.js
+++ creativecheck/src/social-fix.js
@@ -3,13 +3,18 @@ const facebookSvg='<svg viewBox="0 0 24 24" aria-hidden="true"><path fill="curre
 const linkedinSvg='<svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M6.5 8.5H3V21h3.5V8.5ZM4.75 3A2.05 2.05 0 1 0 4.75 7.1 2.05 2.05 0 0 0 4.75 3ZM21 13.85c0-3.77-2.01-5.53-4.69-5.53-2.16 0-3.13 1.19-3.67 2.03V8.5H9.15V21h3.49v-6.2c0-1.63.31-3.21 2.33-3.21 1.99 0 2.02 1.87 2.02 3.33V21H21v-7.15Z"/></svg>';
 function fixSocial(){
   const box=document.querySelector('.footer-social');
-  if(!box || box.dataset.fixed==='1') return;
-  box.dataset.fixed='1';
+  if(!box) return;
   const links=[...box.querySelectorAll('a')];
-  if(links[0]){links[0].href='https://www.instagram.com/creativecheck.app/';links[0].target='_blank';links[0].rel='noreferrer';links[0].ariaLabel='CreativeCheck on Instagram';links[0].innerHTML=instagramSvg;}
-  if(links[1]){links[1].href='https://www.linkedin.com/company/creativecheck/';links[1].target='_blank';links[1].rel='noreferrer';links[1].ariaLabel='CreativeCheck on LinkedIn';links[1].innerHTML=linkedinSvg;}
-  if(links[2]){links[2].href='https://www.facebook.com/creativecheck.app/';links[2].target='_blank';links[2].rel='noreferrer';links[2].ariaLabel='CreativeCheck on Facebook';links[2].innerHTML=facebookSvg;}
-  if(!links[2]){const a=document.createElement('a');a.className='footer-social-icon';a.href='https://www.facebook.com/creativecheck.app/';a.target='_blank';a.rel='noreferrer';a.ariaLabel='CreativeCheck on Facebook';a.innerHTML=facebookSvg;box.appendChild(a);}
+  const data=[
+    ['https://www.instagram.com/creativecheck.app/','CreativeCheck on Instagram',instagramSvg],
+    ['https://www.linkedin.com/company/creativecheck/','CreativeCheck on LinkedIn',linkedinSvg],
+    ['https://www.facebook.com/creativecheck.app/','CreativeCheck on Facebook',facebookSvg]
+  ];
+  data.forEach(([href,label,svg],i)=>{
+    let a=links[i];
+    if(!a){a=document.createElement('a');a.className='footer-social-icon';box.appendChild(a);}
+    a.href=href;a.target='_blank';a.rel='noreferrer';a.ariaLabel=label;a.innerHTML=svg;
+  });
 }
 new MutationObserver(fixSocial).observe(document.documentElement,{childList:true,subtree:true});
 fixSocial();
